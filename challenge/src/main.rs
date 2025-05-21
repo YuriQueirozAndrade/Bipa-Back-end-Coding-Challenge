c6308761da53926baeec25734fa9540ec2ffc73f
Yuri <yuriqueirozandradeoficial@gmail.com>
2025-05-21 14:33:20 -0300

    feat: add retrive_node function and node struct

--- challenge/src/main.rs
+++ challenge/src/main.rs
@@ -1,3 +1,24 @@
+use reqwest::blocking::{Client, Response};
+use serde::{Deserialize, Serialize};
+
+#[derive(Debug, Serialize, Deserialize)]
+pub struct Node {
+    #[serde(rename = "publicKey")]
+    pub_key: String,
+    alias: String,
+    capacity: u64,
+    #[serde(rename = "firstSeen")]
+    first_seen: i64,
+}
+fn retrive_node() -> Response {
+    Client::new()
+        .get("https://mempool.space/api/v1/lightning/nodes/rankings/connectivity")
+        .send()
+        .expect("Could not get data from the endpoint")
+}
+
 fn main() {
-    println!("Hello, world!");
+    let nodes: Vec<Node> = retrive_node().json().expect("Failed to parse JSON");
+
+    println!("{:?}", nodes);
 }
