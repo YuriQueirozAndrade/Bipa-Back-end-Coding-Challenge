2e30923d72cb26aa2e212ea01669669dc7cc9331
Yuri <yuriqueirozandradeoficial@gmail.com>
2025-05-22 11:21:17 -0300

    feat: add serializer to capacity and timestamp

--- challenge/src/main.rs
+++ challenge/src/main.rs
@@ -1,3 +1,4 @@
+use chrono::{TimeZone, Utc};
 use reqwest::blocking::{Client, Response};
 use rusqlite::Connection;
 use serde::{Deserialize, Serialize};
@@ -7,10 +8,31 @@ pub struct Node {
     #[serde(rename = "publicKey")]
     pub_key: String,
     alias: String,
+
+    #[serde(rename = "capacity")]
+    #[serde(serialize_with = "s_capacity")]
     capacity: u64,
+
     #[serde(rename = "firstSeen")]
+    #[serde(serialize_with = "s_timestamp")]
     first_seen: i64,
 }
+
+fn s_capacity<S>(capacity: &u64, serializer: S) -> Result<S::Ok, S::Error>
+where
+    S: serde::Serializer,
+{
+    serializer.serialize_f64(*capacity as f64 / 100_000_000.0)
+}
+
+fn s_timestamp<S>(timestamp: &i64, serializer: S) -> Result<S::Ok, S::Error>
+where
+    S: serde::Serializer,
+{
+    let datetime = Utc.timestamp_opt(*timestamp, 0).unwrap();
+    serializer.serialize_str(&datetime.to_rfc3339())
+}
+
 fn retrive_node() -> Response {
     Client::new()
         .get("https://mempool.space/api/v1/lightning/nodes/rankings/connectivity")
@@ -28,6 +50,7 @@ fn create_db() -> Connection {
 }
 fn insert_db(conn: &Connection, nodes: Vec<Node>) {
     for node in &nodes {
+        // Future improvment: try put all in one sql query,dont use the for loop
         conn.execute(
             "INSERT INTO node (pubkey, alias, capacity, first_seen) VALUES (?1, ?2, ?3, ?4)
             ON CONFLICT(pubkey) DO UPDATE SET capacity = EXCLUDED.capacity, first_seen = EXCLUDED.first_seen",
@@ -37,6 +60,8 @@ fn insert_db(conn: &Connection, nodes: Vec<Node>) {
     }
 }
 fn retrive_db(conn: &Connection) -> Vec<Node> {
+    // Future improvment:keep the json file in memorie, if data base donts update, send the in
+    // memmori json, if update, make a new query
     let mut stmt = conn
         .prepare("SELECT pubkey, alias, capacity, first_seen FROM node")
         .expect("Error on prepare the sql query");
@@ -53,9 +78,12 @@ fn retrive_db(conn: &Connection) -> Vec<Node> {
     let nodes: Vec<Node> = node_inter.filter_map(Result::ok).collect();
     nodes
 }
+
 fn main() {
     let nodes: Vec<Node> = retrive_node().json().expect("Failed to parse JSON");
     let db = create_db();
     insert_db(&db, nodes);
-    println!("{:?}", retrive_db(&db));
+    let json =
+        serde_json::to_string_pretty(&retrive_db(&db)).expect("Failed to convert data to JSON");
+    println!("{}", json);
 }
