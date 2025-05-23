33df8a6e535b7ddb4d8a5c495e3d6e12caf57e01
Yuri <yuriqueirozandradeoficial@gmail.com>
2025-05-23 04:42:35 -0300

    fix: split the database operations of the main.rs

---
+++ challenge/src/db_ops.rs
@@ -0,0 +1,42 @@
+use crate::node::Node;
+use rusqlite::Connection;
+
+pub fn create_db() -> Connection {
+    let conn = Connection::open("./nodes.db").expect("Could not start a connection with data base");
+    conn.execute(
+        "CREATE TABLE IF NOT EXISTS node ( pubkey TEXT PRIMARY KEY, alias TEXT, capacity INTEGER, first_seen INTEGER)",
+        (),
+    )
+    .expect("Error on create the table on db");
+    conn
+}
+pub fn insert_db(conn: &Connection, nodes: Vec<Node>) {
+    for node in &nodes {
+        // Future improvment: try put all in one sql query,dont use the for loop
+        conn.execute(
+            "INSERT INTO node (pubkey, alias, capacity, first_seen) VALUES (?1, ?2, ?3, ?4)
+            ON CONFLICT(pubkey) DO UPDATE SET capacity = EXCLUDED.capacity, first_seen = EXCLUDED.first_seen",
+        (&node.pub_key, &node.alias, node.capacity, node.first_seen),
+    )
+    .expect("Could not insert into node table");
+    }
+}
+pub fn retrive_db(conn: &Connection) -> Vec<Node> {
+    // Future improvment:keep the json file in memorie, if data base donts update, send the in
+    // memmori json, if update, make a new query
+    let mut stmt = conn
+        .prepare("SELECT pubkey, alias, capacity, first_seen FROM node")
+        .expect("Error on prepare the sql query");
+    let node_inter = stmt
+        .query_map([], |row| {
+            Ok(Node {
+                pub_key: row.get(0).expect("Could not get pub_key"),
+                alias: row.get(1).expect("Could not get alias"),
+                capacity: row.get(2).expect("Could not get capacity"),
+                first_seen: row.get(3).expect("Could not get first_seen"),
+            })
+        })
+        .expect("Not is possible make the query for retrive all data from db");
+    let nodes: Vec<Node> = node_inter.filter_map(Result::ok).collect();
+    nodes
+}
--- challenge/src/main.rs
+++ challenge/src/main.rs
@@ -1,3 +1,4 @@
+use challenge::db_ops::{create_db, insert_db, retrive_db};
 use challenge::node::Node;
 use reqwest::blocking::{Client, Response};
 use rusqlite::Connection;
@@ -14,45 +15,6 @@ fn retrive_node() -> Response {
         .get("https://mempool.space/api/v1/lightning/nodes/rankings/connectivity")
         .send()
         .expect("Could not get data from the endpoint")
-}
-fn create_db() -> Connection {
-    let conn = Connection::open("./nodes.db").expect("Could not start a connection with data base");
-    conn.execute(
-        "CREATE TABLE IF NOT EXISTS node ( pubkey TEXT PRIMARY KEY, alias TEXT, capacity INTEGER, first_seen INTEGER)",
-        (),
-    )
-    .expect("Error on create the table on db");
-    conn
-}
-fn insert_db(conn: &Connection, nodes: Vec<Node>) {
-    for node in &nodes {
-        // Future improvment: try put all in one sql query,dont use the for loop
-        conn.execute(
-            "INSERT INTO node (pubkey, alias, capacity, first_seen) VALUES (?1, ?2, ?3, ?4)
-            ON CONFLICT(pubkey) DO UPDATE SET capacity = EXCLUDED.capacity, first_seen = EXCLUDED.first_seen",
-        (&node.pub_key, &node.alias, node.capacity, node.first_seen),
-    )
-    .expect("Could not insert into node table");
-    }
-}
-fn retrive_db(conn: &Connection) -> Vec<Node> {
-    // Future improvment:keep the json file in memorie, if data base donts update, send the in
-    // memmori json, if update, make a new query
-    let mut stmt = conn
-        .prepare("SELECT pubkey, alias, capacity, first_seen FROM node")
-        .expect("Error on prepare the sql query");
-    let node_inter = stmt
-        .query_map([], |row| {
-            Ok(Node {
-                pub_key: row.get(0).expect("Could not get pub_key"),
-                alias: row.get(1).expect("Could not get alias"),
-                capacity: row.get(2).expect("Could not get capacity"),
-                first_seen: row.get(3).expect("Could not get first_seen"),
-            })
-        })
-        .expect("Not is possible make the query for retrive all data from db");
-    let nodes: Vec<Node> = node_inter.filter_map(Result::ok).collect();
-    nodes
 }
 
 fn db_updater(db: Arc<Mutex<Connection>>) {
