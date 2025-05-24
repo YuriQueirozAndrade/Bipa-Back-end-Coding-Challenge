545cf9053a0d0c688c5e9d3b7eb4d9f7aa6410b1
Yuri <yuriqueirozandradeoficial@gmail.com>
2025-05-24 14:26:54 -0300

    feat (dp_ops): add print menssages on error heandler
    feat (dp_osp): add a verify if the nodes array is empy on insert

--- challenge/src/db_ops.rs
+++ challenge/src/db_ops.rs
@@ -2,6 +2,7 @@ use crate::network::retrive_node;
 use crate::node::{Cache, Node};
 use rusqlite::{params, Connection, Result};
 use std::{
+    fmt,
     sync::{Arc, Mutex},
     thread,
     time::Duration,
@@ -15,6 +16,22 @@ pub enum DbError {
     UpdateError,
 }
 
+impl fmt::Display for DbError {
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        write!(
+            f,
+            "{}",
+            match self {
+                DbError::CreateError => "CreateError",
+                DbError::InsertError => "InsertError",
+                DbError::RetriveError => "RetriveError",
+                DbError::UpdateError => "UpdateError",
+            }
+        )
+    }
+}
+
+
 pub fn create_db() -> Result<Connection, DbError> {
     match Connection::open("./nodes.db"){
         Ok(conn) => {
@@ -22,16 +39,28 @@ pub fn create_db() -> Result<Connection, DbError> {
                 "CREATE TABLE IF NOT EXISTS node (pubkey TEXT PRIMARY KEY, alias TEXT, capacity INTEGER, first_seen INTEGER)",()
             ) {
                 Ok(_) => Ok(conn),
-                Err(_) => Err(DbError::CreateError),
+                Err(e) => {
+                    eprintln!("Error on create table: {}",e);
+                    Err(DbError::CreateError)
+                }
             }
         }
-        Err(_) => Err(DbError::CreateError),
+        Err(e) => {
+            eprintln!("Error on connection: {}",e);
+            Err(DbError::CreateError)
+        }
     }
 }
 pub fn insert_db(conn: &mut Connection, nodes: Vec<Node>) -> Result<(), DbError> {
+    if nodes.is_empty(){
+        return Ok(());
+    }
     let tx = match conn.transaction() {
         Ok(transaction) => transaction,
-        Err(_) => return Err(DbError::InsertError),
+        Err(e) => {
+            eprintln!("Error on create transaction: {}", e);
+            return Err(DbError::InsertError);
+        }
     };
     {
         let mut stmt = match  tx.prepare(
@@ -39,7 +68,10 @@ pub fn insert_db(conn: &mut Connection, nodes: Vec<Node>) -> Result<(), DbError>
             ON CONFLICT(pubkey) DO UPDATE SET capacity = EXCLUDED.capacity, first_seen = EXCLUDED.first_seen"
             ) {
             Ok(statement) => statement,
-            Err(_) => return  Err(DbError::InsertError),
+            Err(e) => {
+                eprintln!("Error on prepare statament: {}", e);
+                return  Err(DbError::InsertError)
+            } 
         };
         for node in &nodes {
             match stmt.execute(params![
@@ -49,20 +81,29 @@ pub fn insert_db(conn: &mut Connection, nodes: Vec<Node>) -> Result<(), DbError>
                 node.first_seen,
             ]) {
                 Ok(_) => (),
-                Err(_) => return Err(DbError::InsertError),
+                Err(e) => {
+                    eprintln!("Error on execute statament: {}", e);
+                    return Err(DbError::InsertError)
+                } 
             }
         }
     }
     match tx.commit() {
         Ok(_) => Ok(()),
-        Err(_) => Err(DbError::InsertError),
+        Err(e) => {
+            eprintln!("Error on commit transaction: {}",e);
+            Err(DbError::InsertError)
+        }
     }
 }
 
 pub fn retrive_db(conn: &Connection) -> Result<Vec<Node>, DbError> {
     let mut stmt = match conn.prepare("SELECT pubkey, alias, capacity, first_seen FROM node") {
         Ok(stmt) => stmt,
-        Err(_) => return Err(DbError::RetriveError),
+        Err(e) => {
+            eprintln!("Error on execute statament: {}", e);
+            return Err(DbError::RetriveError)
+        } 
     };
     match stmt.query_map([], |row| {
         Ok(Node {
@@ -73,7 +114,10 @@ pub fn retrive_db(conn: &Connection) -> Result<Vec<Node>, DbError> {
         })
     }) {
         Ok(node_iter) => Ok(node_iter.filter_map(Result::ok).collect()),
-        Err(_) => Err(DbError::RetriveError),
+        Err(e) => {
+            eprintln!("Error on retribe nodes: {}", e);
+            Err(DbError::RetriveError)
+        } 
     }
 }
 
@@ -85,19 +129,31 @@ pub fn db_updater(
         {
             let mut locked_db = match db.lock() {
                 Ok(locked_db) => locked_db,
-                Err(_) => return DbError::UpdateError,
+                Err(e) => {
+                    eprintln!("Error on lock database: {}", e);
+                    return DbError::UpdateError
+                } 
             };
             let mut cache_lock = match node_cache.lock() {
                 Ok(cache) => cache,
-                Err(_) => return DbError::UpdateError,
+                Err(e) => {
+                    eprintln!("Error on lock cache nodes: {}", e);
+                    return DbError::UpdateError
+                } 
             };
             let nodes: Vec<Node> = match retrive_node().json() {
                 Ok(nodes) => nodes,
-                Err(_) => return DbError::UpdateError,
+                Err(e) => {
+                    eprintln!("Error on call retrive_node: {}", e);
+                    return DbError::UpdateError
+                } 
             };
             match insert_db(&mut locked_db, nodes) {
                 Ok(_) => cache_lock.expired = true,
-                Err(_) => return DbError::UpdateError,
+                Err(e) => {
+                    eprintln!("Error on call insert_db: {}", e);
+                    return DbError::UpdateError
+                } 
             };
         }
         println!("Database Update");
